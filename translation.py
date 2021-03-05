428ca207a17526134e75ebe5881106b615058b21
DragoLK <69932259+Drago991@users.noreply.github.com>
2021-03-05 14:58:58 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -10,7 +10,7 @@ class Translation(object):
 
 ⭕️ Support: <a href="https://t.me/LK9779">My_master</a>
 
-⭕️ Follow and Join our channels and groups. \n<a href="https://t.me/slfilms">FilⓂ️s Hub™️〽️</a> | <a href="https://t.me/seriez_hub">𝕊𝕖𝕣𝕚𝕖𝕤 ℍ𝕌𝔹🏷™️〽️</a>
+⭕️ Follow and Join our channels and groups. \n<a href="https://t.me/joinchat/RKuoSl5fBXbmpZJM">FilⓂ️s Hub™️〽️</a> | <a href="https://t.me/joinchat/UVScphiXLM6vQH7q">𝕊𝕖𝕣𝕚𝕖𝕤 ℍ𝕌𝔹🏷™️〽️</a>
    
    <b>Note:</b> I'm running on a free server so there maybe some issues like not responing, So please enjoy the service patiently."""
 
